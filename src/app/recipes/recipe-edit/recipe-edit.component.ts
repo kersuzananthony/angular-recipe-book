193771b74ec96fcdae9e522da4fc944604d3638a
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2017-01-28 11:21:50 +0100

    Adds recipe form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {RecipeService} from "../recipe.service";
+import {Subscription} from "rxjs";
+import {Recipe} from "../recipe";
 
 @Component({
   selector: 'rb-recipe-edit',
   templateUrl: './recipe-edit.component.html'
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subscription: Subscription;
+  private isNew: boolean = true;
+  private recipeIndex: number;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.subscription = this.activatedRoute.params.subscribe(
+      (params: any) => {
+        if (params.hasOwnProperty('id')) {
+          this.recipeIndex = +params['id'];
+          this.isNew = false;
+        } else {
+          this.isNew = true;
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
