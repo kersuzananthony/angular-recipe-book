062276fd601c6b329fbac8a477c5cf1437b089b6
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2017-01-30 11:23:08 +0100

    Reorganizes code in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
+import {CoreModule} from "./core.module";
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 
 import {RecipeService} from "./recipes/recipe.service";
 import {ShoppingService} from "./shopping-list/shopping.service";
+
 import {routing} from "./app.routing";
-import {CoreModule} from "./core.module";
 
 @NgModule({
   declarations: [
